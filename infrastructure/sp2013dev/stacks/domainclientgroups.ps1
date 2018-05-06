4c830a8b7df0b9af75632c6f0bff13564ffc9d21
Aleks <alexander@sapozhkov.net>
2018-05-06 22:53:55 +0100

    testable web vm provisioning

--- infrastructure/sp2013dev/stacks/domainclientgroups.ps1
+++ infrastructure/sp2013dev/stacks/domainclientgroups.ps1
@@ -1,4 +1,4 @@
-$configName = "DomainClient"
+$configName = "DomainClientGroups"
 Configuration $configName
 {
     param(
@@ -9,33 +9,16 @@ Configuration $configName
     )
 
     Import-DscResource -ModuleName PSDesiredStateConfiguration
-    Import-DSCResource -ModuleName xComputerManagement -ModuleVersion 3.2.0.0
-    Import-DscResource -ModuleName xNetworking -ModuleVersion 5.6.0.0
 
     Node $AllNodes.NodeName
     {        
 
-        xDefaultGatewayAddress SetDefaultGateway 
-        { 
-            Address        = '192.168.52.1' 
-            InterfaceAlias = 'Ethernet 2' 
-            AddressFamily  = 'IPv4' 
-        }
-        
-        xDnsServerAddress DnsServerAddress
+        Group AdminGroup
         {
-            InterfaceAlias = 'Ethernet 2'
-            AddressFamily  = 'IPv4'
-            Address        = '192.168.52.128'
-            Validate       = $false
-        }
- 
-        xComputer JoinDomain
-        {
-            Name        = $NodeName
-            DomainName  = "contoso.local"
-            Credential  = $DomainAdminCredential
-            DependsOn   = "[xDnsServerAddress]DnsServerAddress"
+            GroupName           = "Administrators"
+            Credential          = $DomainAdminCredential
+            MembersToInclude    = "contoso\OG SharePoint2016 Server Admin Prod"
+            DependsOn           = "[xComputer]JoinDomain"
         }
 
     }
