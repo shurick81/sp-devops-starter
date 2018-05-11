d7afae91ab7c8ea4c35a77d4b859ed2a38b2dcfa
Aleks <alexander@sapozhkov.net>
2018-05-11 16:30:03 +0100

    renaming main-ci stack and adding preprovisioning

---
+++ infrastructure/sp2013dev/stacks/joinslave-vb.ps1
@@ -0,0 +1,2 @@
+Invoke-RestMethod -Uri "https://repo.jenkins-ci.org/releases/org/jenkins-ci/plugins/swarm-client/3.5/swarm-client-3.5.jar" -OutFile "swarm-client-3.5.jar";
+Start-Process -FilePath "java" -ArgumentList "-jar swarm-client-3.5.jar -name $env:computername -disableSslVerification -master http://192.168.52.80:8080 -username admin -password admin -labels `"sharepoint-auto`" -executors 1";
