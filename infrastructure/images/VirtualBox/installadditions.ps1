e716cc9abfa14c08f23c3c73f8e15cea5b694a4e
SAPOZHKOV Aleksandr <aleksandr.sapozhkov@soprasteria.com>
2018-07-23 22:34:35 +0200

    softer certificate installation

--- infrastructure/images/VirtualBox/installadditions.ps1
+++ infrastructure/images/VirtualBox/installadditions.ps1
@@ -1,4 +1,6 @@
 e:\cert\VBoxCertUtil add-trusted-publisher e:\cert\vbox-sha1.cer --root e:\cert\vbox-sha1.cer
 e:\cert\VBoxCertUtil add-trusted-publisher e:\cert\vbox-sha256.cer --root e:\cert\vbox-sha256.cer
-e:\cert\VBoxCertUtil add-trusted-publisher e:\cert\vbox-sha256-r3.cer --root e:\cert\vbox-sha256-r3.cer
+if ( Get-Item e:\cert\vbox-sha256-r3.cer -ErrorAction Ignore ) {
+    e:\cert\VBoxCertUtil add-trusted-publisher e:\cert\vbox-sha256-r3.cer --root e:\cert\vbox-sha256-r3.cer
+}
 e:\VBoxWindowsAdditions.exe /S
