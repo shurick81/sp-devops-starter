be4adc2cd27be1b6639fdbe81a4abd2f1aaaa818
SAPOZHKOV Aleksandr <aleksandr.sapozhkov@soprasteria.com>
2018-07-25 14:03:09 +0200

    added vagrant azure plugin into VM provisioning

---
+++ infrastructure/stacks/vagrantplugins.ps1
@@ -0,0 +1,3 @@
+vagrant plugin install vagrant-azure;
+Rename-Item -Path C:\Users\vagrant\.vagrant.d\gems\2.4.4\gems\vagrant-azure-2.0.0 -NewName "vagrant-azure-2.0.0 - Copy";
+git clone https://github.com/shurick81/vagrant-azure C:\Users\vagrant\.vagrant.d\gems\2.4.4\gems\vagrant-azure-2.0.0;
