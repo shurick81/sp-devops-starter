96b57f07107ed8d325bdb5556a362ec5a11266c7
SAPOZHKOV Aleksandr <aleksandr.sapozhkov@soprasteria.com>
2018-07-28 08:11:03 +0200

    moving vagrant plugin installation from image to provisioning

--- infrastructure/stacks/vagrantplugins.ps1
+++ infrastructure/stacks/vagrantplugins.ps1
@@ -1,3 +1,4 @@
 vagrant plugin install vagrant-azure;
 Rename-Item -Path C:\Users\vagrant\.vagrant.d\gems\2.4.4\gems\vagrant-azure-2.0.0 -NewName "vagrant-azure-2.0.0 - Copy";
 git clone https://github.com/shurick81/vagrant-azure C:\Users\vagrant\.vagrant.d\gems\2.4.4\gems\vagrant-azure-2.0.0;
+vagrant plugin install vagrant-reload;
